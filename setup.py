da54dbb3b0707209666276851abd7b303204ef42
Akshay <akshaypramesh4@gmail.com>
2026-06-28 17:05:27 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="DrBot",
+    version="0.0.0",
+    author="Akshay",
+    author_email="akshaypramesh4@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
